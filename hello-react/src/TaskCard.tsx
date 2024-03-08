0a46807e3b7af165acd4ddb85129a8d4b7e7c17a
ksvvr <ksvvreddy@gmail.com>
2024-03-08 22:25:11 +0530

    migrated taskcard code from js to ts interface

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,23 +1,57 @@
 /* eslint-disable react/prop-types */
+import React from 'react'
 import './TaskCard.css'
+import { title } from 'process'
+
+interface TaskCardd {
+    title: string
+    assigneeName: string
+}
+
+class dueCard implements TaskCardd {
+    title: string
+    assigneeName: string
+    dueDate: string
+
+    constructor(title:string, assigneeName:string, dueDate: string){
+        this.title = title
+        this.assigneeName = assigneeName
+        this.dueDate = dueDate
+    }
+}
+
+class completedCard implements TaskCardd {
+    title: string
+    assigneeName: string
+    completedAtDate: string
+
+    constructor(title:string, assigneeName:string, completedAtDate: string){
+        this.title = title
+        this.assigneeName = assigneeName
+        this.completedAtDate = completedAtDate
+    }
+}
+
 const TaskCard = (props) => {
     if (props.dueDate != null){
+        const item = new dueCard(props.title, props.assigneeName, props.dueDate)
         return (
             <>
                 <div className='TaskItem mt-3 m-3'>
-                    <h1 className='font-bold text-3xl'>{props.title}</h1>
-                    <p>Due on: {props.dueDate}</p>
-                    <p>Assignee: {props.assigneeName}</p>
+                    <h1 className='font-bold text-3xl'>{item.title}</h1>
+                    <p>Due on: {item.dueDate}</p>
+                    <p>Assignee: {item.assigneeName}</p>
                 </div>
             </>
         )
     } else{ 
+        const item = new completedCard(props.title, props.assigneeName, props.completedAtDate)
         return (
             <>
                 <div className='TaskItem mt-3 m-3'>
-                    <h1 className='font-bold text-3xl'>{props.title}</h1>
-                    <p>Completed on: {props.completedAtDate}</p>
-                    <p>Assignee: {props.assigneeName}</p>
+                    <h1 className='font-bold text-3xl'>{item.title}</h1>
+                    <p>Completed on: {item.completedAtDate}</p>
+                    <p>Assignee: {item.assigneeName}</p>
                 </div>
             </>
         )
