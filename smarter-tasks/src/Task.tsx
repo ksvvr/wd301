410d35b08b274b536d19450351fc67e7f5a183b9
ksvvr <ksvvreddy@gmail.com>
2024-04-03 20:09:10 +0530

    m4 try 1

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -27,11 +27,14 @@ interface TaskProp {
 
 const Task = (props: TaskItem) => {
   return (
-    <div className="TaskItem shadow-md border border-slate-100">
-      <h2 className="text-base font-bold my-1">{props.title}</h2>
-      <p className="text-sm text-slate-500">{props.dueDate}</p>
-      <p className="text-sm text-slate-500">Description: {props.description}</p>
-    </div>
+    <li>
+      <div className="TaskItem shadow-md border border-slate-100">
+        <h2 className="text-base font-bold my-1">{props.title}</h2>
+        <p className="text-sm text-slate-500">{props.dueDate}</p>
+        <p className="text-sm text-slate-500">Description: {props.description}</p>
+        <button className="deleteTaskButton">Delete</button>
+      </div>
+    </li>
   );
 };
 
