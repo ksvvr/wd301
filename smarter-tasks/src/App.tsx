5bc17a58edeb8a669a09e2495087406607a65820
ksvvr <ksvvreddy@gmail.com>
2024-04-17 18:33:57 +0530

    m6 midway

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,59 +1,33 @@
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import TaskListPage from "./pages/TaskListPage";
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-import Signin from "./pages/Signin";
-import ProtectedRoute from "./ProtectedRoute";
-import Layout from "./Layout";
-import NotFound from './pages/Notfound';
-
+import Notfound from "./pages/Notfound";
+import Signup from './pages/signup';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/signin" replace />,
+    element: <Signup />,
   },
   {
-    path: "/signin",
-    element: <Signin />,
+    path: "/signup",
+    element: <Signup />,
   },
   {
-    element: (
-      <ProtectedRoute>
-        <Layout />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      },
-      {
-        path: "tasks",
-        element: <TaskListPage />,
-      },
-      {
-        path: "tasks/:id",
-        element: <TaskDetailsPage />,
-      },
-      {
-        path: "notfound",
-        element: <NotFound />,
-      },
-    ],
+    path: "/notfound",
+    element: <Notfound />,
   },
   {
     path: "*",
-    element: <Navigate to="/notfound" replace />,
-  },
+    element: <Notfound />,
+  }
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
-};
+  return (
+    <RouterProvider router={router} />
+  );
+}
 
-export default App;
+export default App
