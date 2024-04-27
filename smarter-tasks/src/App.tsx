f1be319fd853a6b66b8d232fe109db876319e9b1
ksvvr <ksvvreddy@gmail.com>
2024-04-27 18:25:07 +0530

    members are being displayed

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -3,18 +3,17 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes"
 import { ThemeContext } from "./context/theme";
-
-// To do that, first I'll import the `ProjectsProvider` in the `App` component.
-
 import { ProjectsProvider } from "./context/projects/context";
+import { MembersProvider } from "./context/members/context"; // Import the MembersProvider
 
-// Then I'll wrap the RouterProvider component with the <ProjectsProvider> component.
 const App = () => {
   const { theme } = useContext(ThemeContext)
   return (
      <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
       <ProjectsProvider>
-        <RouterProvider router={router} />
+        <MembersProvider> {/* Nest MembersProvider inside ProjectsProvider */}
+          <RouterProvider router={router} />
+        </MembersProvider>
       </ProjectsProvider>
     </div>
   );
