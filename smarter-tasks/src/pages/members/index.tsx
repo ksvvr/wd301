1e2d0049fc8a0ee39c0d685a19cc468d40a76692
ksvvr <ksvvreddy@gmail.com>
2024-04-27 18:08:39 +0530

    updated Members page

--- smarter-tasks/src/pages/members/index.tsx
+++ smarter-tasks/src/pages/members/index.tsx
@@ -1,7 +1,27 @@
+import React, { useEffect } from 'react';
+import { useMembersState, useMembersDispatch } from '../../context/members/context';
+import { fetchMembers } from '../../context/members/actions';
+import MemberListItems from './MemberListItems';
+
 const Members = () => {
-    return (
-      <h2>Members</h2>
-    )
-  }
-  export default Members;
-  
+  const dispatch = useMembersDispatch();
+  const { members, isLoading, error } = useMembersState();
+
+  useEffect(() => {
+    fetchMembers(dispatch);
+  }, [dispatch]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading members!</p>;
+
+  return (
+    <div>
+      <h1>Members List</h1>
+      {members.map((member) => (
+        <MemberListItems key={member.id} member={member} onDelete={() => {}} />
+      ))}
+    </div>
+  );
+};
+
+export default Members;
