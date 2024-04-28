929794c12d380622cd63803cc74f370e64120e45
ksvvr <ksvvreddy@gmail.com>
2024-04-28 18:45:23 +0530

    m7 5

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -42,10 +42,10 @@ export default function MemberListItems() {
   return (
     <>
       {members.map((member: any) => (
-        <div key={member.id} className=".member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div key={member.id} className="member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">{member.name}</h5>
           <h5 className="mb-2 text font-small tracking-tight text-gray-900 dark:text-white">{member.email}</h5>
-          <button className=".member">Delete</button>
+          <button>Delete</button>
         </div>
       ))}        
     </>
